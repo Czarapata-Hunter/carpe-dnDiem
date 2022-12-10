6b39c66cdb3c472684855d4dbe6603cbf7fc5f76
GeoSauer <geoffrey.sauer89@gmail.com>
2022-12-09 19:02:38 -0700

    more refactoring

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
-const { Events } = require('discord.js');
+// const { Events } = require('discord.js');
 
 module.exports = {
-  name: Events.ClientReady,
+  name: 'ready',
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
