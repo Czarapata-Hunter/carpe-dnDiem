d34cf46c23e7122471ab0ad2863ef6cf3d2ffbce
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-12-12 16:58:24 -0700

    adding barbarian results for testing

--- sql/setup.sql
+++ sql/setup.sql
@@ -86,3 +86,19 @@ CREATE TABLE wizard(
   die_value INTEGER NOT NULL,
   result VARCHAR NOT NULL,
 ); 
+
+INSERT INTO barbarian (die_value, result) VALUES
+(1, 'In the midst of your rage you shrivel up and start to cry'),
+(1, 'They said you were too mad to die and yet'),
+(2, 'while charging fearlessly you become timid and deal a low blow of damage'),
+(2, 'Be thankful for your natural damage resistance today'),
+(2, 'STOP CLEAVE'),
+(3, 'During your attack you notice a butterfly and decide to stare at it for a while'),
+(3, 'Not everything needs to die'),
+(3, 'Take some deep breaths before you try anything crazy'),
+(4, 'your strength was enough to not get pushed over'),
+(4, 'You start hittin and you aint quittin'),
+(4, 'CLEAVE'),
+(5, 'Your blow dealt massive damage and everyone loves you'),
+(5, 'Your weapon is only a feathers weight on this day'),
+(5, 'Your enemies should make themselves unfindable if they wish to see tomorrow'),
